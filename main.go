bbb192d41b7e3061b2f866b157ac7192df67c1e6
Johny-Wood <91822495+Johny-Wood@users.noreply.github.com>
2023-12-12 16:08:53 +0300

    fix: fix resolvig config

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
   "fmt"
   "log"
   "os"
-  "path"
+  // "path"
   "path/filepath"
   "regexp"
   "strings"
@@ -37,19 +37,109 @@ type Translation struct {
 
 
 func resolveFilePath(filename string) (string, error) {
-  ex, err := os.Executable()
-  if err != nil {
-    return "", err
-  }
+  // Get the directory of the current Go file
+	goFileDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return "", err
+	}
 
-  dir := filepath.Dir(ex)
-  exPath := filepath.Dir(ex)
-  if strings.Contains(dir, "go-build") {
-    return filename, nil 
-  } else {
-    filePath := path.Join(exPath, filename)
-    return filePath, nil
-  }
+	// Define the filename you want to read
+	// filename := "example.txt"
+
+	// Construct the file path in the current directory
+	filePath := filepath.Join(goFileDir, filename)
+
+	// Check if the file exists in the current directory
+	_, err = os.Stat(filePath)
+	if os.IsNotExist(err) {
+		// If file doesn't exist in current directory, try to use os.Executable()
+		execPath, err := os.Executable()
+		if err != nil {
+			fmt.Println("Error getting executable path:", err)
+			return "", err
+		}
+
+		execDir := filepath.Dir(execPath)
+		filePath = filepath.Join(execDir, filename)
+
+		// Check again if the file exists using the executable directory
+		_, err = os.Stat(filePath)
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist in the current directory or in the directory of the executable.")
+			return "", err
+		}
+	}
+  
+  return filePath, nil
+ //  // Get the path of the running executable
+	// execPath, err := os.Executable()
+	// if err != nil {
+	// 	fmt.Println("Error getting executable path:", err)
+	// 	return "", err
+	// }
+	//
+	// // Derive the directory of the executable
+	// execDir := filepath.Dir(execPath)
+	//
+	// // Define the filename you want to read
+	// // filename := "example.txt"
+	//
+	// // Construct the full file path relative to the executable's directory
+	// filePath := filepath.Join(execDir, filename)
+	//
+	// // Check if the file exists
+	// _, err = os.Stat(filePath)
+	// if os.IsNotExist(err) {
+	// 	fmt.Println("File does not exist in the directory of the executable.")
+	// 	return "", err
+	// }
+	//
+ //  return filePath, nil
+ // //  // Get the current working directory
+	// // workingDir, err := os.Getwd()
+	// // if err != nil {
+	// // 	fmt.Println("Error getting working directory:", err)
+	// // 	return "", err
+	// // }
+	// //
+	// // // Define the filename you want to read
+	// // // filename := "example.txt"
+	// //
+	// // // Check if the file exists in the current directory
+	// // filePath := filepath.Join(workingDir, filename)
+	// // _, err = os.Stat(filePath)
+	// //
+	// // if os.IsNotExist(err) {
+	// // 	fmt.Println("File does not exist in the current directory.")
+	// // 	return "", err
+	// // }
+ // //  
+	// // // Open and read the file
+ // //  return filePath, nil 
+	// // // file, err := os.Open(filePath)
+	// // // if err != nil {
+	// // // 	fmt.Println("Error opening file:", err)
+	// // // 	return
+	// // // }
+	// // // defer file.Close()
+	// //
+	// // // Read file contents
+	// // // ... (Your file reading logic here)
+	// // // fmt.Println("Reading file:", filename)
+ // //  // ex, err := os.Executable()
+ // //  // if err != nil {
+ // //  //   return "", err
+ // //  // }
+ // //  //
+ // //  // dir := filepath.Dir(ex)
+ // //  // exPath := filepath.Dir(ex)
+ // //  // if strings.Contains(dir, "go-build") {
+ // //  //   return filename, nil 
+ // //  // } else {
+ // //  //   filePath := path.Join(exPath, filename)
+ // //  //   return filePath, nil
+ // //  // }
 }
 
 func SplitAny(s string, seps string) []string {
@@ -323,19 +413,19 @@ func main() {
   }
 
   // Get config file path
-  // configFilePath, err := resolveFilePath(formatter.configPath)
-  // if err != nil {
-  //   panic(err)
-  // }
+  configFilePath, err := resolveFilePath(formatter.configPath)
+  if err != nil {
+    panic(err)
+  }
 
 
   // Try to read and decode CONFIG file
-  cPath, err := filepath.Glob("config.toml")
-  // if _, err := os.Stat(configFilePath); !os.IsNotExist(err) {
+  // cPath, err := filepath.Glob("./*.toml")
+  if _, err := os.Stat(configFilePath); !os.IsNotExist(err) {
 
-  if _, err := os.Stat(cPath[0]); !os.IsNotExist(err) {
-    // fileContents, err := os.ReadFile(configFilePath)
-    fileContents, err := os.ReadFile(cPath[0])
+  // if _, err := os.Stat(cPath[0]); !os.IsNotExist(err) {
+    fileContents, err := os.ReadFile(configFilePath)
+    // fileContents, err := os.ReadFile(cPath[0])
     if err != nil {
       fmt.Printf("Error while reading config.toml file. Error - %s. Continue working with default setting.", err)
     } else {
